9cedfaca49629b5b7627472f3f2307227756851c
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-12 18:28:56 +0530

    Chagin Variable n to num

--- Week_3/fizzbuzz.py
+++ Week_3/fizzbuzz.py
@@ -1,6 +1,6 @@
 #Fizzbuzz Code
-n=input("Enter a Maximum Range= ") #Enter the Maximum Range to check if it is Fizz or Buzz or FizzBuzz
-n=int(n)
+num=input("Enter a Maximum Range= ") #Enter the Maximum Range to check if it is Fizz or Buzz or FizzBuzz
+num=int(num)
 
 def fizzbuzz(n:int):
     if(n%3==0 or n%5 == 0):
@@ -14,7 +14,7 @@ def fizzbuzz(n:int):
         print(n,"is None")
 
 def main():
-    for i in range(n):
+    for i in range(num):
         fizzbuzz(i)
 
 if __name__ == "__main__":
