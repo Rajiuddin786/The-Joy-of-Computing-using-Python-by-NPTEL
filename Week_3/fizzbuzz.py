7ac7c727dbeb1a377540533d502357892b24b071
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-12 18:26:07 +0530

    Done

---
+++ Week_3/fizzbuzz.py
@@ -0,0 +1,21 @@
+#Fizzbuzz Code
+n=input("Enter a Maximum Range= ") #Enter the Maximum Range to check if it is Fizz or Buzz or FizzBuzz
+n=int(n)
+
+def fizzbuzz(n:int):
+    if(n%3==0 or n%5 == 0):
+        if(n%3 == 0 and n%5 == 0):
+            print(n,"is FizzBuzz")
+        elif n%3 == 0:
+            print(n,"is Fizz")
+        else:
+            print(n,"is Buzz")
+    else:
+        print(n,"is None")
+
+def main():
+    for i in range(n):
+        fizzbuzz(i)
+
+if __name__ == "__main__":
+    main()
