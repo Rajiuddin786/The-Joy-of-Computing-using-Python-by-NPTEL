13fc721998c67e48859217949b1862b9828d6fbb
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-15 10:51:48 +0530

    Added for loop factorial

--- Week_6/factorial.py
+++ Week_6/factorial.py
@@ -1,9 +1,25 @@
-def fac(n):
+def fac_recursion(n):
     if n<=0:
-        return f'Wrong Input'
-    if n == 1:
-        return 1
-    return n*fac(n-1)
+        print('Wrong Input')
+    else:
+        if n == 1:
+            return 1
+        return n*fac_recursion(n-1)
+
+def fac_normally(n):
+    if n<=0:
+        print('Wrong Input')
+    else:
+        f=1
+        for i in range(1,n+1):
+            f*=i
+        return f
 
 n=int(input("Enter a number to find the Factorial: "))
-print(fac(n))
+fac_rec=fac_recursion(n)
+fac_nor=fac_normally(n)
+
+if fac_rec:
+    print("Factorial Recursively=",fac_rec)
+if fac_nor:
+    print("Factorial Normally=",fac_nor)
