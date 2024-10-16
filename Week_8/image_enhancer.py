8d2db7fe8bb9143d33258bef927665a8f754b4f3
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-16 19:01:18 +0530

    Adding Image Enhacement

--- Week_8/image_enhancer.py
+++ Week_8/image_enhancer.py
@@ -1,13 +1,33 @@
-#Flipping the Image
 from PIL import Image
+import cv2
 
-img = Image.open('./Files/image_zoro.jpg')
+#Flipping the Image
+def flipping():
+    img = Image.open('./Files/image_zoro.jpg')
 
-#Transposing
-transposed_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    #Transposing
+    transposed_img = img.transpose(Image.FLIP_LEFT_RIGHT)
 
-#Save it to a understandable level of Human
+    #Save it to a understandable level of Human
 
-transposed_img.save('./Files/new_img.png')
+    transposed_img.save('./Files/new_img.png')
 
-print('Done Flipping')
+    print('Done Flipping')
+
+#Image Enhancement
+def enhance():
+    img=cv2.imread('./Files/image_zoro.jpg')
+
+    #Preparation of CLAHE
+    clahe = cv2.createCLAHE()
+
+    #Convert img into grayscale image
+    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    #Apply Enhacement to gray_img
+    enh_img=clahe.apply(gray_img)
+
+    #Output
+    cv2.imwrite('./Files/new_img.png',enh_img)
+
+enhance()
