17f0f5223406f2c28d0676fbe708a3a1845054ff
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-16 18:34:11 +0530

    Adding Vizulization using Matplotlib

--- Week_8/lottery_simulation.py
+++ Week_8/lottery_simulation.py
@@ -1,20 +1,28 @@
 from random import randint
+import matplotlib.pyplot as mlt
 
 account = 0
-count = 0
-while True:
+count = 1
+x=[]
+y=[]
+while count<366:
+    x.append(count)
     bet=randint(1,10)
-    print("You bet on:",bet)
+    # print("You bet on:",bet)
     lucky_draw = randint(1,10)
-    print("The result is",lucky_draw)
+    #print("The result is",lucky_draw)
 
 
     if bet == lucky_draw:
         account +=800
     else:
         account -=100
-    c = input("Do you want to continue y/n: ").capitalize()
-    if c == 'N':
-        break
+    # c = input("Do you want to continue y/n: ").capitalize()
+    # if c == 'N':
+    #     break
+    y.append(account)
+    count+=1
 
 print("Your Amount:",account)
+mlt.plot(x,y)
+mlt.show()
