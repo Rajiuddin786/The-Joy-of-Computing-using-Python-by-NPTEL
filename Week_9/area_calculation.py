1e65e828a67b3d5e826d4dacaa31de5eaac256ae
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-20 17:42:18 +0530

    Solve using Method 2

--- Week_9/area_calculation.py
+++ Week_9/area_calculation.py
@@ -22,24 +22,51 @@ import random
 
 # print(r,g,b)
 
-img=scipy.misc.imread("map_01.png")
+"""Method - 01"""
+
+# img=scipy.misc.imread("map_01.png")
+# count_pun=0
+# count_ind=0
+# count=0
+
+# while count<=20000:
+#     x=random.randint(0,2735)
+#     y=random.randint(0,2480)
+#     z=0
+
+#     if img[x][y][z] == 60:
+#         count_ind+=1
+#         count+=1
+#     else:
+#         if img[x][y][z] == 80:
+#             count_pun+=1
+#         count+=1
+
+# area_pun=(count_pun/count_ind)*32287263
+
+# print(area_pun)
+
+
+img=Image.open('map-01.png')
+rgb_img=img.convert("RGB")
+
+count_in=0
 count_pun=0
-count_ind=0
 count=0
 
-while count<=20000:
-    x=random.randint(0,2735)
-    y=random.randint(0,2480)
+while count<=10000:
+    x=random.randint(0,2480)
+    y=random.randint(0,2735)
     z=0
-
-    if img[x][y][z] == 60:
-        count_ind+=1
+    r,g,b=rgb_img.getpixel((x,y))
+    if r==60:
+        count_in += 1
         count+=1
     else:
-        if img[x][y][z] == 80:
+        if r == 80:
             count_pun+=1
         count+=1
 
-area_pun=(count_pun/count_ind)*32287263
+area_pun=(count_pun/count_in)*3287263
 
 print(area_pun)
